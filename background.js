1fbfbc87e445e53ba5fefbc7d8c3dbd1491fcc96
zezang <zezang@gmail.com>
2022-07-15 22:58:52 -0700

    finished persistent state

--- background.js
+++ background.js
@@ -22,25 +22,9 @@
 //     })
 // })
 
-chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: [{
-      'id': 1001,
-      'priority': 1,
-      'action': {
-        'type': 'redirect',
-        'redirect': {
-          url: 'https://www.facebook.com'
-        }
-      },
-      'condition': {
-        'urlFilter': 'https://www.twitter.com',
-        'resourceTypes': [
-          'csp_report', 'font', 'image', 'main_frame', 'media', 'object', 'other', 'ping', 'script',
-          'stylesheet', 'sub_frame', 'webbundle', 'websocket', 'webtransport', 'xmlhttprequest'
-        ]
-      }
-    }],
-   removeRuleIds: [1001]
-  })
+chrome.storage.sync.get('restricted_Updated', function(arr) {
+  console.log(arr['restricted_Updated'])
+})
+
 
 
--- popup.js
+++ popup.js
@@ -10,9 +10,10 @@ function createElement(parent, contents, type) {
     return element;
   }
 
-
 document.addEventListener('DOMContentLoaded', () => {
     let restrictedArr = ['youtube.com', 'facebook.com', 'twitter.com'];
+    chrome.storage.sync.set({'restricted_Original': restrictedArr})
+    // let restrictedArr = ['youtube.com', 'facebook.com', 'twitter.com'];
     //declare a constant and set it equal to the "restricted sites" at bottom of popup
     const restricted = document.querySelector('#restricted-sites');
     //set target element to the hidden element with the restricted websites
@@ -23,22 +24,63 @@ document.addEventListener('DOMContentLoaded', () => {
         else targetEl.style.display = "flex"
     })
 
+    //first query the parent restricted container we will append our urls to on the DOM
     const restricted_container = document.querySelector('#restrictedurl-container');
-    for (const url of restrictedArr) {
-        const urlDiv = createElement(restricted_container, url, 'div');
-        urlDiv.setAttribute('class', 'restricted-div')
-    }
-    
+    //use chrome.storage.sync to get the stored array of all banned urls
+    chrome.storage.sync.get('restricted_Updated', function(arr) {
+        //of the restricted_Updated property does not exists, call get on the original array and clone it to the updated array
+        if (!arr['restricted_Updated']) {
+            chrome.storage.sync.get('restricted_Original', function(arr) {
+                //iterate through the original array and create divs for url and icon
+                for (const url of arr['restricted_Original']) {
+                    const urlContainer = createElement(restricted_container, '', 'div');
+                    urlContainer.setAttribute('class', 'banned-container')
+                    const urlDiv = createElement(urlContainer, url, 'div');
+                    urlDiv.setAttribute('class', 'restricted-div');
+                    const minus = createElement(urlContainer, '', 'button');
+                    minus.setAttribute('class', 'remove-icon');
+                    minus.addEventListener('click', function(){
+                        alert('REMOVE DIV?')
+                    })
+                }
+                chrome.storage.sync.set({'restricted_Updated': restrictedArr});
+            })
+            return;
+        }
+        //otherwise, iterate through the updated array and set up the divs
+        else {
+            for (const url of arr['restricted_Updated']) {
+                const urlContainer = createElement(restricted_container, '', 'div');
+                urlContainer.setAttribute('class', 'banned-container')
+                const urlDiv = createElement(urlContainer, url, 'div');
+                urlDiv.setAttribute('class', 'restricted-div');
+                const minus = createElement(urlContainer, '-', 'button');
+                minus.setAttribute('class', 'remove-icon');
+                minus.addEventListener('click', function(){
+                    alert('REMOVE DIV?')
+                })
+            }
+        }
+    });
+    //add event listener to the input form
     document.getElementById('blockform-div').addEventListener('submit', function(e) {
+        //prevent page reload
         e.preventDefault();
+        //get the value of the input form, if it's not empty then push the new url into the array
+        //create a new div container with the url as the innerHTML
+        //update the chrome storage
         const inputText = document.getElementById('blockform').value;
         if (inputText) {
-            restrictedArr.push(inputText);
+            //create the new div with the entered url in the input form
             const urlDiv = createElement(restricted_container, inputText, 'div');
             urlDiv.setAttribute('class', 'restricted-div');
+            //clear the contents of the input form
             document.getElementById('blockform').value = null;
+            //get the storage property of restricted_Updated, and set it with the inputText added
+            chrome.storage.sync.get('restricted_Updated', function(arr){
+                chrome.storage.sync.set({'restricted_Updated': [...arr['restricted_Updated'], inputText]})
+            })
         }
-        // alert(document.getElementById('blockform').value)
     });
 })
 
